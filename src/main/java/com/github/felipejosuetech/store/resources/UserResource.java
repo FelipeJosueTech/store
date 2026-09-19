fac10a78563f2a78e5cff217a7c30eff4ca59e4c
Felipe Batista <felipe.cbatista@outlook.com>
2026-09-19 19:28:03 -0300

    User entity end resource

---
+++ src/main/java/com/github/felipejosuetech/store/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.github.felipejosuetech.store.resources;
+
+import com.github.felipejosuetech.store.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "9999999");
+        return ResponseEntity.ok().body(u);
+    }
+}
